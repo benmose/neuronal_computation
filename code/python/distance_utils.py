b923188d19cba74d8498478c02070b9676f1df43
Ram Mantzur <benmose@gmail.com>
2023-07-07 04:33:47 +0300

    stability diagram 1

--- code/python/distance_utils.py
+++ code/python/distance_utils.py
@@ -3,30 +3,34 @@ import numpy as np
 from scipy.optimize import curve_fit
 from utils import read_coordinates_of_unstable_point_from_dat, shortes_distance_quadratic_func
 
-def frequency2distance(file_name, dir_path, func): 
+def frequency2distance(file_name, dir_path, dval, func, n=7): 
     filename = os.path.join(dir_path, file_name)
     basename = os.path.splitext(filename)[0]
     outfile = os.path.join(dir_path, basename + ".txt")
 
-    dtmp, ftmp = read_coordinates_of_unstable_point_from_dat(filename, 0, 1, 7)
+    dtmp, ftmp = read_coordinates_of_unstable_point_from_dat(filename, 0, 1, n)
     distmp = []
 
     C2, C1, C0 = (0.97248748,0.69684267,-0.00626603)
 
-    f = lambda x: C0 + C1 * x + C2 * pow(x, 2)
+    #f = lambda x: C0 + C1 * x + C2 * pow(x, 2)
     
 
     with open(outfile, "w") as f:
         for i in range(len(dtmp)):
             disttemp = shortes_distance_quadratic_func(0.02, dtmp[i], C2, C1, C0)
             distmp.append(disttemp)
-            f.write(r'%f, %f, %f' % (0.02, dtmp[i], disttemp))
+            f.write(r'%f, %f, %f' % (dval, dtmp[i], disttemp))
             f.write('\n')
 
     d = np.array(dtmp)
     f = np.array(ftmp)
     dist = np.array(distmp)
 
-    popt, _ = curve_fit(func, dist, f)
-    return dist, f, popt
+    popt , _ = curve_fit(func, dist, f)  
+    return d, f, dist, popt
 
+
+def frequency_approx(d, f, func):
+    popt , _ = curve_fit(func, d, f)  
+    return  popt
--- code/python/ditance_curves.py
+++ code/python/ditance_curves.py
@@ -2,41 +2,73 @@ import os
 import numpy as np
 from config import dat_dir_path
 import matplotlib.pyplot as plt
-from distance_utils import frequency2distance
+from distance_utils import frequency2distance, frequency_approx
 
 dir_name = "frequency"
 dir_path = os.path.join(dat_dir_path, dir_name)
 
 func = lambda x,a,b,c: a*np.sqrt(x+c) + b*(x+c)
 
-dist, f, popt = frequency2distance("z_0_02_s_0_001.dat", dir_path, func)
-dist1, f1, popt1 = frequency2distance("z_0_05_s_0_01.dat", dir_path, func)
+func1 = lambda x,a,b: a*np.sqrt(x) + b*(x)
+
+
+d, f, dist, popt = frequency2distance("z_0_02_s_0_001.dat", dir_path, 0.02, func)
+d1, f1, dist1, popt1 = frequency2distance("z_0_05_s_0_01.dat", dir_path, 0.05, func)
+d2, f2, dist2, popt2 = frequency2distance("z_0_07_s_0_01.dat", dir_path, 0.07, func, 8)
+
+shifted_d = d-0.00824477
+shifted_d1 = d1-0.0298903
+shifted_d2 = d2-0.0463035
+popta = frequency_approx(shifted_d, f, func1)
 
 plt.figure()
-plt.plot(dist, f)
-plt.plot(dist, func(dist, *popt))
-plt.plot(dist1, f1)
-plt.plot(dist1, func(dist1, *popt1))
+plt.plot(d,f)
+plt.plot(d1, f1)
+plt.plot(d2, f2)
+plt.title('frequency vs d')
 
-plt.title('frequency vs distance')
+# s = (r'%1.5f*$\sqrt{x+%1.5f}$ + %1.5f*(x+%1.5f)'%
+#           popt)
 
-s = (r'%1.5f*$\sqrt{x+%1.5f}$ + %1.5f*(x+%1.5f)'%
-          (popt[0],
-           popt[2],
-           popt[1],
-           popt[2]))
+# s1 = (r'%1.5f*$\sqrt{x+%1.5f}$ + %1.5f*(x+%1.5f)'%
+#           popt1)
 
-s1 = (r'%1.5f*$\sqrt{x+%1.5f}$ + %1.5f*(x+%1.5f)'%
-          (popt1[0],
-           popt1[2],
-           popt1[1],
-           popt1[2]))
+plt.legend(['z=0.02', 'z=0.05', 'z=0.07'])
 
-plt.legend(['original data z=0.02',
-            s,
-            'original data z=0.05',
-            s1])
-plt.xlabel('distance')
+# plt.legend(['original data z=0.02',
+#             s,
+#             'original data z=0.05',
+#             s1])
+# plt.xlabel('distance')
+plt.xlabel('d')
+plt.ylabel('frequency')
+plt.xlim(0, 1)
+plt.ylim(0, 0.6)
+plt.show()
+
+plt.figure()
+plt.plot(shifted_d,f)
+plt.plot(shifted_d1, f1)
+plt.plot(shifted_d2, f2)
+plt.plot(shifted_d, func1(shifted_d, *popta))
+
+
+
+s = (r'fit = %1.5f*$\sqrt{x}$ + %1.5f*(x)'% tuple(popta))
+
+plt.title(s)
+
+# s1 = (r'%1.5f*$\sqrt{x+%1.5f}$ + %1.5f*(x+%1.5f)'%
+#           popt1)
+
+plt.legend(['z=0.02', 'z=0.05', 'z=0.07', 'fitted curve'])
+
+# plt.legend(['original data z=0.02',
+#             s,
+#             'original data z=0.05',
+#             s1])
+# plt.xlabel('distance')
+plt.xlabel('d')
 plt.ylabel('frequency')
 plt.xlim(0, 1)
 plt.ylim(0, 0.6)
