2c9f96d0fad4f46d56f606a6610cb3886cb0761c
Ram Mantzur <benmose@gmail.com>
2023-07-28 13:43:06 +0300

    refactor rate_model_functions

--- code/python/rate_model_calculations/rate_model_functions.py
+++ code/python/rate_model_calculations/rate_model_functions.py
@@ -1,4 +1,5 @@
 import numpy as np
+import rate_model_coeffs
 import matplotlib.pyplot as plt
 
 def l(x):
@@ -8,71 +9,92 @@ def l(x):
 def dl(x1,y1):
     return -(0.91149071 * x1 -1 * y1 -0.01253742) / (np.sqrt((0.91149071 * 0.91149071) + 1))
 
-def d_biff_iapp_1(x):
-    return 0.972*(x**2)+0.69684*x-0.00627
-
-def Freq_iapp_1(z,d):
-    return 0.73515848*np.sqrt(d-d_biff_iapp_1(z))-0.30205245*(d-d_biff_iapp_1(z))
-
-def M_iapp_1(z, d):
-    if d>=d_biff_iapp_1(z):
-        return (Freq_iapp_1(z,d))
-    return (0)
-
-
-def d_biff_iapp_2(x):
-    return 0.90313*(x**2)+0.70472*x-0.01208
-
-def Freq_iapp_2(z,d):
-    return 0.73484029*np.sqrt(d-d_biff_iapp_2(z))-0.30128275*(d-d_biff_iapp_2(z))
-
-def M_iapp_2(z, d):
-    if d>=d_biff_iapp_2(z):
-        return (Freq_iapp_2(z,d))
-    return (0)
-
-def d_biff_iapp_3(x):
-    return 0.91151*(x**2)+0.69966*x-0.01766
-
-def Freq_iapp_3(z,d):
-    return 0.73389634*np.sqrt(d-d_biff_iapp_3(z))-0.30225673*(d-d_biff_iapp_3(z))
-
-def M_iapp_3(z, d):
-    if d>=d_biff_iapp_3(z):
-        return (Freq_iapp_3(z,d))
-    return (0)
+class RateModelFunctions:
+    def __init__(self, d_biff_coeff = [], 
+                 freq_app_coeff = [], 
+                 currents_values = []):
+        self.d_biff_coeff = d_biff_coeff
+        self.freq_app_coeff = freq_app_coeff
+        self.current_values = currents_values
+        self.__d_biff_func = lambda x,a,b,c: a*(x**2) + b*x + c
+        self.__freq_func = lambda x,a,b: a*np.sqrt(x) + b*x
 
 
-def d_biff_iapp_4(x):
-    return 0.71915*(x**2)+0.75968*x-0.02728
+    def d_biff_iapp(self, index, x):
+        assert index < len(self.d_biff_coeff)
+        return self.__d_biff_func(x,
+                                  self.d_biff_coeff[index][0],
+                                  self.d_biff_coeff[index][1],
+                                  self.d_biff_coeff[index][2])
+    
+    def print_d_biff_iapp(self, index, x):
+        print(r'%f*(%f**2) + %f*%f + %f' % (self.d_biff_coeff[index][0],
+                                         x,
+                                         self.d_biff_coeff[index][1],
+                                         x,
+                                         self.d_biff_coeff[index][2]))
+    
+    def freq_iapp(self, index, z, d):
+        assert index < len(self.freq_app_coeff)
+        return self.__freq_func(d-self.d_biff_iapp(index,z),
+                                self.freq_app_coeff[index][0],
+                                self.freq_app_coeff[index][1])
+    
+    def print_freq_iapp(self, index, z, d):
+        print(r'%f*np.sqrt(%f) + %f*%f'%(
+            self.freq_app_coeff[index][0],
+             d-self.d_biff_iapp(index,z),
+             self.freq_app_coeff[index][1],
+             d-self.d_biff_iapp(index,z)))
+    
+    def M_iapp(self, index, z, d):
+        if d >= self.d_biff_iapp(index,z):
+            return self.freq_iapp(index, z, d)
+        return(0)
+    
+    def add_to_list(self, a, l):
+        l.append(a)
 
-def Freq_iapp_4(z,d):
-    return 0.73403684*np.sqrt(d-d_biff_iapp_4(z))-0.30451244*(d-d_biff_iapp_4(z))
+    def plot_freq_equations(self, zval):
+        legend = []
+        d = np.linspace(0, 1)
 
-def M_iapp_4(z, d):
-    if d>=d_biff_iapp_4(z):
-        return (Freq_iapp_4(z,d))
-    return (0)
+        for i in range(len(self.current_values)):
+            legend.append(r'iApp = %dmA' % (self.current_values[i]))
+        plt.figure()
+        for i in range(len(self.freq_app_coeff)):
+            plt.plot(d, self.freq_iapp(i, zval, d))
+
+        plt.title('frequency vs d')
+
+        plt.legend(legend)
+
+        plt.xlabel('d')
+        plt.ylabel('val on line')
+        plt.xlim(0, 1)
+        plt.ylim(0, 1)
+        plt.show()
+
+    def plot_saddel_point_equations(self):
+        legend = []
+        d = np.linspace(0, 1)
+
+        for i in range(len(self.current_values)):
+            legend.append(r'iApp = %dmA' % (self.current_values[i]))
+        plt.figure()
+        for i in range(len(self.d_biff_coeff)):
+            plt.plot(d, self.d_biff_iapp(i, d))
+
+        plt.title('d vs z')
+
+        plt.legend(legend)
+
+        plt.xlabel('d')
+        plt.ylabel('z')
+        plt.xlim(0, 1)
+        plt.ylim(0, 1)
+        plt.show()
 
 
-d = np.linspace(0, 1)
-print("d_biff_app_2(0.0105927): ", d_biff_iapp_2(0.0105927))
-#print("d_biff_app_4(0.07): ", d_biff_iapp_4(0.07))
+                                 
 
-print('Freq_2 0 0.01: ', Freq_iapp_1(0, 0.01))
-
-def plot_equations(z_val):
-    plt.figure()
-    plt.plot(d,Freq_iapp_1(z_val, d))
-    plt.plot(d, Freq_iapp_2(z_val,d))
-    plt.plot(d, Freq_iapp_4(z_val,d))
-
-    plt.title('frequency vs d')
-
-    plt.legend(['iapp = 1mA', 'iapp = 2mA', 'iapp = 4mA'])
-
-    plt.xlabel('d')
-    plt.ylabel('frequency')
-    plt.xlim(0, 1)
-    plt.ylim(0, 0.6)
-    plt.show()
