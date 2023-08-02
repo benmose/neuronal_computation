Add limit to comparison module plot function. Add function for average distance between two lines. Correct the d_biff coefficients. Add average distance frequency between two frequency equations. Start documenting frequencies values for comparison.

code/python/rate_model_calculations/rate_model_functions.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 utils_path = pathlib.Path(__file__).parent.parent.joinpath("utils").as_posix()
 sys.path.append(utils_path)
 
-from distance_utils import frequency_approx
+from distance_utils import frequency_approx, averaged_distance
 
 
 
@@ -107,20 +107,28 @@ class RateModelFunctions:
         self.averaged_d_biff_func_coeffs = popt
         return popt
 
+    def average_distance_freq(self, ind1, ind2, zval):
+        assert ind1 < len(self.freq_app_coeff)
+        assert ind2 < len(self.freq_app_coeff)
+        x = np.linspace(0.1, 1)
+        y1 = self.freq_iapp(ind1, zval, x)
+        y2 = self.freq_iapp(ind2, zval, x)
+        return averaged_distance(x, y1, x, y2)
+
     def plot_freq_equations(self, zval):
         legend = []
         d = np.linspace(0, 1)
 
         for i in range(len(self.current_values)):
             legend.append(r'iApp = %dmA' % (self.current_values[i]))
-        legend.append('averaged freq values')
-        legend.append('aveeraged freq equation')
+        #legend.append('averaged freq values')
+        #legend.append('aveeraged freq equation')
         plt.figure()
         for i in range(len(self.freq_app_coeff)):
             plt.plot(d, self.freq_iapp(i, zval, d))
 
-        plt.plot(d, self.averaged_freq_values(zval,d))
-        plt.plot(d, self.averaged_freq(zval,d))
+        #plt.plot(d, self.averaged_freq_values(zval,d))
+        #plt.plot(d, self.averaged_freq(zval,d))
 
 
 

code/python/rate_model_calculations/unstable_points_curves.py
@@ -9,7 +9,7 @@ dat_path = pathlib.Path(__file__).parent.parent.parent.parent.joinpath("dat").as
 sys.path.append(utils_path)
 sys.path.append(dat_path)
 
-from distance_utils import frequency_approx_scaled_quad, frequency_approx
+from distance_utils import frequency_approx
 from utils import read_coordinates_from_dat
 
 class UnstablePointsCurve:
@@ -35,7 +35,35 @@ class UnstablePointsCurve:
         self.lpld = np.array(self.lply)
 
     def plot_curves(self, maxx=0.2, maxy=0.2):
-        func = lambda x,a,b,c: a*(x**2) + b*(x) + c
+        func = lambda x,a,b,c,d: a*((x+d)**2) + b*(x+d) + c
+        func1 = lambda x,a,b,c: a*((x)**2) + b*(x) + c
+        popt = frequency_approx(self.lplz, self.lpld, func1)
+
+        plt.figure()
+        plt.plot(self.hz, self.hd)
+        plt.plot(self.lplz, self.lpld)
+        plt.plot(self.lplz, func1(self.lplz, *popt))
+
+
+
+        s = (r'fit = (%1.5f*$(x)^2$ + %1.5f*(x) +%1.5f)'% tuple(popt))
+        print(popt)
+        print("max z: ", max(self.lplz))
+        print("max d: ", max(self.lpld))
+
+        plt.title(s)
+
+        plt.legend(['bifurcation', 'saddle', 'fitted curve'])
+
+        plt.xlabel('z')
+        plt.ylabel('d')
+        plt.xlim(0, maxx)
+        plt.ylim(0, maxy)
+        plt.show()
+
+    def plot_curves_with_coeffs(self, maxx=0.2, maxy=0.2, coeffs = []):
+        func = lambda x,a: coeffs[0]*((x+a)**2) + coeffs[1]*(x+a) + coeffs[0]
+        func1 = lambda x,a,b,c: a*((x)**2) + b*(x) + c
         popt = frequency_approx(self.lplz, self.lpld, func)
 
         plt.figure()
@@ -45,7 +73,7 @@ class UnstablePointsCurve:
 
 
 
-        s = (r'fit = (%1.5f*$x^2$ + %1.5f*(x) +%1.5f)'% tuple(popt))
+        s = (r'fit = (%1.5f*$(x)^2$ + %1.5f*(x) +%1.5f)'% tuple(popt))
         print(popt)
         print("max z: ", max(self.lplz))
         print("max d: ", max(self.lpld))